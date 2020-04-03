4fd5e48dccc689fe0448a117b1217a7a75acdb2f
Hugo Ferreira <hgferreirag@gmail.com>
2020-04-03 02:43:28 -0300

    feat: Add installation scripts

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+HOME_FOLDER=$(awk -v FS=':' -v user="$USER" '($1==user) {print $6}' "/etc/passwd")
+DIR="$( cd "$( dirname "$(readlink "${BASH_SOURCE[0]}")" )" >/dev/null 2>&1 && pwd )"
+
+remove_hookit_to_path(){
+	rm $HOME_FOLDER/bin/hookit
+}
+
+remove_hookit_to_path
