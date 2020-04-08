56846e45c948dfa6fdeda930e56ee686bf97a17b
Hugo Ferreira <hgferreirag@gmail.com>
2020-04-08 02:01:43 -0300

    fix(installer): Fix dir path usage
    Getting the correct dir name to allow usage from anywhere

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 HOME_FOLDER=$(awk -v FS=':' -v user="$USER" '($1==user) {print $6}' "/etc/passwd")
-DIR="$( cd "$( dirname "$(readlink "${BASH_SOURCE[0]}")" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 
 remove_hookit_to_path(){
 	rm $HOME_FOLDER/bin/hookit
