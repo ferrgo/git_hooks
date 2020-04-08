28e2442ffb702b1f52ab09a58d949e19a791fbf9
Hugo Ferreira <hgferreirag@gmail.com>
2020-04-08 02:01:43 -0300

    fix(installer): Fix dir path usage
    Getting the correct dir name to allow usage from anywhere

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 HOME_FOLDER=$(awk -v FS=':' -v user="$USER" '($1==user) {print $6}' "/etc/passwd")
-DIR="$( cd "$( dirname "$(readlink "${BASH_SOURCE[0]}")" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 add_hookit_to_path(){
 	ln -s $DIR/hookit $HOME_FOLDER/bin/hookit
